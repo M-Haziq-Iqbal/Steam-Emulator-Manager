b057d50fc3c18f81151692db2b3c17c3647ba46c
Haziq <mhaziqiqbal01@gmail.com>
2024-01-29 05:33:19 +0800

    create steam_interface.txt

--- generate_emu_config.bat
+++ generate_emu_config.bat
@@ -1,7 +1,7 @@
 @echo off
 
 ::setup variables::
-set "appid=1290000"
+set "appid="
 set "USERNAME=KurashiAOI"
 set "PASSWORD=rnRv8Gu#QB3NJbF!ety$"
 
@@ -38,10 +38,19 @@ if not defined PASSWORD (
 )
 
 ::setup constant
-set "dll_file_goldberg=Goldberg_Lan_Steam_Emu_master--475342f0\experimental\steam_api.dll"
-set "dll64_file_goldberg=Goldberg_Lan_Steam_Emu_master--475342f0\experimental\steam_api64.dll"
-set "dll_file_backup=..\backup\steam_api(%appid%).dll"
-set "dll64_file_backup=..\backup\steam_api64(%appid%).dll"
+
+if exist "..\steam_api.dll" (
+	set "dll_ver=steam_api.dll"
+	set "dll_file_original=..\steam_api.dll"
+	set "dll_file_goldberg=Goldberg_Lan_Steam_Emu_master--475342f0\experimental\steam_api.dll"
+	set "dll_file_backup=..\backup\steam_api(%appid%).dll"
+) else if exist "..\steam_api64.dll" (
+	set "dll_ver=steam64_api.dll"
+	set "dll_file_original=..\steam_api64.dll"
+	set "dll_file_goldberg=Goldberg_Lan_Steam_Emu_master--475342f0\experimental\steam_api64.dll"
+	set "dll_file_backup=..\backup\steam_api64(%appid%).dll"
+)
+
 set "script_file=goldberg_emulator-master-scripts\scripts\generate_emu_config.py"
 set "interfaces_exe=Goldberg_Lan_Steam_Emu_master--475342f0\tools\generate_interfaces_file.exe"
 
@@ -49,45 +58,51 @@ set "interfaces_exe=Goldberg_Lan_Steam_Emu_master--475342f0\tools\generate_inter
 if exist "%appid%_output" ( rd /s /q "%appid%_output" )
 if exist "login_temp" ( rd /s /q "login_temp" )
 if exist "..\steam_settings" ( rd /s /q "..\steam_settings" )
-if exist "steam_interfaces.txt" ( del "steam_interfaces.txt" )
 if exist "..\steam_interfaces.txt" ( del "..\steam_interfaces.txt" )
-if exist "%dll_file_backup%" ( copy "%dll_file_backup%" "..\steam_api.dll" > nul )
-if exist "%dll64_file_backup%" ( copy "%dll64_file_backup%" "..\steam_api64.dll" > nul )
+if exist "%dll_file_backup%" ( copy "%dll_file_backup%" "%dll_file_original%" > nul )
 echo Successfully deleted folders and files from any previous installation!
 
 pause
-
-if exist "..\steam_api.dll" (
-	set "dll_file_original=..\steam_api.dll"
-) else if exist "..\steam_api64.dll" (
-	set "dll_file_original=..\steam_api64.dll"
-)
 
 ::backup original steam_api.dll or steam_api64.dll
 if not exist "%dll_file_backup%" (
 	mkdir "..\backup"
 	move "%dll_file_original%" "%dll_file_backup%" > nul
 	if exist "%dll_file_backup%" (
-		echo Successfully backed up original %appid% steam_api.dll!
+		echo Successfully backed up original %appid% %dll_ver%!
 	)
 )
 
 ::copy steam_api.dll or steam_api64.dll to dll folder
 copy "%dll_file_goldberg%" .. > nul
 if exist "%dll_file_original%" (
-	echo Successfully copied Goldberg steam_api.dll to dll folder!
+	echo Successfully copied Goldberg %dll_ver% to dll folder!
 )
 
-::create emu config::
-call python %script_file% %USERNAME% %PASSWORD% %appid%
-echo Successfully created emu config!
+::create steam_interfaces.txt
+start "" "%interfaces_exe%" "%dll_file_backup%"
 
-::move emu config to game directory::
+:check_running
+timeout /t 1 /nobreak >nul
+tasklist | find /i "%interfaces_exe%" >nul
+if %ERRORLEVEL% equ 0 ( goto :check_running )
+
+::move steam_interfaces.txt to dll folder::
+move "steam_interfaces.txt" .. > nul
+if exist "..\steam_interfaces.txt" (
+	echo Successfully created steam_interfaces.txt in dll folder!
+)
+
+::create emu config:
+call python %script_file% %USERNAME% %PASSWORD% %appid% > nul
+
+::move emu config to game directory
 move "%appid%_output\steam_settings" .. > nul
-echo Successfully moved emu config to game directory!
+echo Successfully created emu config in dll folder!
 
-:delete_leftover_folders
+::delete_leftover_folders
 if exist "%appid%_output" ( rd /s /q "%appid%_output" )
 if exist "login_temp" ( rd /s /q "login_temp" )
+if exist "backup" ( rd /s /q "backup" )
 
 pause

939696fca16fb10b02da412b5700e197a37c03a4
Haziq <mhaziqiqbal01@gmail.com>
2024-01-31 21:49:25 +0800

    error handling | open dll folder | prev_install

--- generate_emu_config.bat
+++ generate_emu_config.bat
@@ -94,7 +94,8 @@ for /f "delims=" %%d in ('dir /ad /b /s ..') do (
 )
 
 if exist "%dll_folder%" (
-	echo DLL directory: %dll_folder%\%dll_file%
+	echo Opening DLL directory: %dll_folder%\%dll_file%
+	start "" "%dll_folder%"
 	echo.
 ) else (
 	echo DLL directory cannot be found!
@@ -110,36 +111,48 @@ set "script_file=goldberg_emulator-master-scripts\scripts\generate_emu_config.py
 set "interfaces_exe=Goldberg_Lan_Steam_Emu_master--475342f0\tools\generate_interfaces_file.exe"
 
 ::delete folders and files from any previous installation
-if exist "%appid%_output" ( rd /s /q "%appid%_output" )
-if exist "login_temp" ( rd /s /q "login_temp" )
-if exist "%dll_folder%\steam_settings" ( rd /s /q "%dll_folder%\steam_settings" )
-if exist "%dll_folder%\steam_interfaces.txt" ( del "%dll_folder%\steam_interfaces.txt" )
-if exist "%dll_file_backup%" ( copy "%dll_file_backup%" "%dll_file_original%" > nul )
-echo Successfully deleted folders and files from any previous installation!
+set "prev_install=1"
 
-pause
-echo.
+if exist "%appid%_output" ( rd /s /q "%appid%_output"
+) else if exist "login_temp" ( rd /s /q "login_temp" 
+) else if exist "%dll_folder%\steam_settings" ( rd /s /q "%dll_folder%\steam_settings" 
+) else if exist "%dll_folder%\steam_interfaces.txt" ( del "%dll_folder%\steam_interfaces.txt" 
+) else if exist "steam_interfaces.txt" ( del "steam_interfaces.txt" 
+) else if exist "%dll_file_backup%" ( copy "%dll_file_backup%" "%dll_file_original%" > nul 
+) else (set "prev_install=0")
+
+if %prev_install%==1 (
+	echo Successfully deleted/restored folders/files from any previous installation!
+	pause
+)
 
 ::create emu config:
 call python %script_file% %accountName% %PASSWORD% %appid% > nul
+if %ERRORLEVEL% neq 0 (
+	echo Unable to run generate_emu_config.py
+	goto :restart
+	)
 
 ::move emu config to game directory
 move "%appid%_output\steam_settings" "%dll_folder%" > nul
 if exist "%dll_folder%\steam_settings" ( echo Successfully created emu config in dll folder! )
 
 ::create steam_interfaces.txt
-start "" "%interfaces_exe%" "%dll_file_original%"
+if exist "%dll_file_original%" (start "" "%interfaces_exe%" "%dll_file_original%")
 
 :check_running
 timeout /t 1 /nobreak >nul
-tasklist | find /i "%interfaces_exe%" >nul
+tasklist | find /i "%interfaces_exe%" > nul
 if %ERRORLEVEL% equ 0 ( goto :check_running )
 
 ::move steam_interfaces.txt to dll folder::
 move "steam_interfaces.txt" "%dll_folder%" > nul
 if exist "%dll_folder%\steam_interfaces.txt" (
 	echo Successfully created steam_interfaces.txt in dll folder!
-)
+) else (
+	echo Unable to create steam_interfaces.txt!
+	goto :restart
+	)
 
 ::backup original steam_api.dll or steam_api64.dll
 if not exist "%dll_file_backup%" (

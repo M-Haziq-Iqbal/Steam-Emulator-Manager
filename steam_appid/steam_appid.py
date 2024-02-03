2210f161a84cb126f60dcb1020d3bb79f562df01
Haziq <mhaziqiqbal01@gmail.com>
2024-02-04 03:59:16 +0800

    refactor | (-) appid only input after name search | (!) script keeps restarting

--- steam_appid/steam_appid.py
+++ steam_appid/steam_appid.py
@@ -29,71 +29,64 @@ async def search_by_appid(app_id): #"https://store.steampowered.com/api/appdetai
         return {'appid': app_data["steam_appid"], 'name': app_data["name"], 'type': app_data["type"]}
     else: return None
 
-async def search_by_game_name(name): #"https://api.steampowered.com/ISteamApps/GetAppList/v0002/"
+async def search_by_game_name(game_name): #"https://api.steampowered.com/ISteamApps/GetAppList/v0002/"
     app_list = await get_app_list()
-    name_normalized = re.sub(r'[^a-zA-Z0-9]', '', name.lower())
+    name_normalized = re.sub(r'[^a-zA-Z0-9]', '', game_name.lower())
     matching_apps = {}
     for app in app_list["applist"]["apps"]:
         app_name_normalized = re.sub(r'[^a-zA-Z0-9]', '', app["name"].lower())
         if name_normalized in app_name_normalized:
             matching_apps[app["appid"]] = app["name"]
-            # matching_apps["appid"] = app["appid"]
     return matching_apps
 
+async def compare_appid(search_query):
+    app_data = await search_by_appid(int(search_query))
+
+    if app_data and app_data.get("type") == "game":
+        print(f'AppID: {search_query} | Name: {app_data["name"]}')
+        return {"appid": app_data["appid"], "name": app_data["name"], "type": app_data["type"]}
+    elif app_data and app_data.get("type") != "game":
+        print(f'AppID: {search_query} | Name: {app_data["name"]}')
+        print(f'Notice: This is not a game')
+    else:
+        print(f"Notice: No game found with ID {search_query}.")
+
+async def compare_game_name(search_query):
+    matching_apps = await search_by_game_name(search_query)
+
+    if matching_apps and len(matching_apps) > 10:
+        print(f"Matching data for '{search_query}' exceeds 10 games. Please enter more specific keywords. ")
+    elif matching_apps:
+        print(f"Matching data for '{search_query}':")
+        tasks = [search_by_appid(app_id) for app_id in matching_apps.keys()] # creates a list of coroutine objects (tasks) using a list comprehension.
+        app_datas = await asyncio.gather(*tasks) # fetch app information for multiple app IDs concurrently.
+        
+        base_game = False
+        for app_id, app_name in matching_apps.items(): #iterates over each key-value pair in the matching_apps dictionary
+            app_data = app_datas.pop(0) #removes and returns the first element from app_datas
+
+            if app_data and app_data["type"] == "game":
+                print(f"AppID: {app_id} | Name: {app_name}")
+                base_game = True
+            elif app_data and not base_game:
+                base_game = False
+
+        if base_game == False:
+            print(f"Error: No base game retrieved")
+    else:
+        print(f"Error: No game found with a name containing '{search_query}'.")
 
 async def main():
-    steam_game = {}
-    while True:
-        search_query = input(f"\nEnter the app ID or name of the app: ")
-        if search_query.isdigit():  # Check if input is only a number
-            app_info = await search_by_appid(int(search_query))
-            if app_info and app_info.get("type") == "game":
-                steam_game = {"appid": search_query, "name": app_info["name"]}
-                print(f'AppID: {search_query} | Name: {app_info["name"]}')
-            elif app_info and app_info.get("type") != "game":
-                print(f'Notice: {app_info["name"]} is not the base game')
-            else:
-                print(f"Notice: No game found with ID {search_query}.")
-        else:
-            matching_apps = await search_by_game_name(search_query)
+    search_query = input(f"\nEnter the app ID or name of the app: ")
 
-            if matching_apps and len(matching_apps) > 10:
-                print(f"\nMatching data for '{search_query}' exceeds 10 games. Please enter more specific keywords: ")
-            
-            elif matching_apps:
-                print(f"\nMatching data for '{search_query}':")
-                tasks = [search_by_appid(app_id) for app_id in matching_apps.keys()] # creates a list of coroutine objects (tasks) using a list comprehension.
-                app_infos = await asyncio.gather(*tasks) # fetch app information for multiple app IDs concurrently.
-                
-                for app_id, app_name in matching_apps.items():
-                    app_info = app_infos.pop(0)
-                    if app_info and app_info["type"] == "game":
-                        print(f"AppID: {app_id} | Name: {app_name}")
-                
-                # search again game data through appid
-                search_query = input(f"\nEnter the app ID of the app: ")
-                
-                if search_query.isdigit():  # Check if input is only a number
-                    app_info = await search_by_appid(int(search_query))
-                    if app_info and app_info.get("type") == "game":
-                        steam_game = {"appid": search_query, "name": app_info["name"]}
-                        print(f'AppID: {search_query} | Name: {app_info["name"]}')
-                    elif app_info and app_info.get("type") != "game":
-                        print(f'Notice: {app_info["name"]} is not the base game')
-                    else:
-                        print(f"Notice: No game found with ID {search_query}.")
-                else: print(f"{search_query} is not a valid AppID")
-
-            else:
-                print(f"Notice: No apps found with a name containing '{search_query}'.")
-        
-        if steam_game:
-            break # Exit the loop
-        else: 
-            print(f"Restarting the script...")
-            continue # Restart the loop
-
-    
+    if search_query.isdigit():  # Check if input is only a number
+        await compare_appid(search_query)
+    else:
+        await compare_game_name(search_query)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    while True:
+        result = asyncio.run(main())
+        if result:
+            print (f"Exiting...")
+            break
